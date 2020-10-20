5d83acb8eb11abee88b7881e3015662dca273835
ghdpreto <ghdpreto@gmail.com>
2020-10-20 00:16:24 -0300

    Ajuste do formulário para captura do dados

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // CSS STYLES
@@ -8,82 +8,125 @@ import '../styles/pages/Register.css';
 import LogoImg from '../assets/images/logo.png';
 
 function Register() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [contact, setContact] = useState('');
+  const [stateUf, setStateUf] = useState('');
+  const [city, setCity] = useState('');
+  const [address, setAddress] = useState('');
+
+
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+    console.log({
+      name,
+      description,
+      contact,
+      stateUf,
+      city,
+      address
+    })
+    
+  }
+
   return (
     <div id="page-register">
-      <Link to="/" >
-        <img src={LogoImg} className="logo-camping-fire" alt="Camping Fire"/>
+      <Link to="/">
+        <img src={LogoImg} className="logo-camping-fire" alt="Camping Fire" />
       </Link>
       <Link to="/">
         <button className="btn-back">Voltar</button>
       </Link>
       <div className="container">
         <h1>Cadastrar</h1>
-        <form>
-          <label>
-            Nome do local
-          </label>
-          <input type="text" placeholder="Digite o nome do local..."/>
+        <form onSubmit={handleSubmit}>
+          <label>Nome do local</label>
+          <input
+            type="text"
+            placeholder="Digite o nome do local..."
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
 
-          <label>
-            Estado
-          </label>
-          <select id="estado" name="estado">
-              <option selected disabled>Selecione o estado</option>
-              <option value="AC">Acre</option>
-              <option value="AL">Alagoas</option>
-              <option value="AP">Amapá</option>
-              <option value="AM">Amazonas</option>
-              <option value="BA">Bahia</option>
-              <option value="CE">Ceará</option>
-              <option value="DF">Distrito Federal</option>
-              <option value="ES">Espírito Santo</option>
-              <option value="GO">Goiás</option>
-              <option value="MA">Maranhão</option>
-              <option value="MT">Mato Grosso</option>
-              <option value="MS">Mato Grosso do Sul</option>
-              <option value="MG">Minas Gerais</option>
-              <option value="PA">Pará</option>
-              <option value="PB">Paraíba</option>
-              <option value="PR">Paraná</option>
-              <option value="PE">Pernambuco</option>
-              <option value="PI">Piauí</option>
-              <option value="RJ">Rio de Janeiro</option>
-              <option value="RN">Rio Grande do Norte</option>
-              <option value="RS">Rio Grande do Sul</option>
-              <option value="RO">Rondônia</option>
-              <option value="RR">Roraima</option>
-              <option value="SC">Santa Catarina</option>
-              <option value="SP">São Paulo</option>
-              <option value="SE">Sergipe</option>
-              <option value="TO">Tocantins</option>
-              <option value="EX">Estrangeiro</option>
+          <label>Estado</label>
+          <select id="estado" name="estado"
+          value={stateUf}
+          onChange={event => setStateUf(event.target.value)}
+          >
+            <option selected disabled>
+              Selecione o estado
+            </option>
+            <option value="AC">Acre</option>
+            <option value="AL">Alagoas</option>
+            <option value="AP">Amapá</option>
+            <option value="AM">Amazonas</option>
+            <option value="BA">Bahia</option>
+            <option value="CE">Ceará</option>
+            <option value="DF">Distrito Federal</option>
+            <option value="ES">Espírito Santo</option>
+            <option value="GO">Goiás</option>
+            <option value="MA">Maranhão</option>
+            <option value="MT">Mato Grosso</option>
+            <option value="MS">Mato Grosso do Sul</option>
+            <option value="MG">Minas Gerais</option>
+            <option value="PA">Pará</option>
+            <option value="PB">Paraíba</option>
+            <option value="PR">Paraná</option>
+            <option value="PE">Pernambuco</option>
+            <option value="PI">Piauí</option>
+            <option value="RJ">Rio de Janeiro</option>
+            <option value="RN">Rio Grande do Norte</option>
+            <option value="RS">Rio Grande do Sul</option>
+            <option value="RO">Rondônia</option>
+            <option value="RR">Roraima</option>
+            <option value="SC">Santa Catarina</option>
+            <option value="SP">São Paulo</option>
+            <option value="SE">Sergipe</option>
+            <option value="TO">Tocantins</option>
+            <option value="EX">Estrangeiro</option>
           </select>
 
-          <label>
-            Cidade
-          </label>
-          <input type="text" placeholder="Digite sua cidade..."/>
+          <label>Cidade</label>
+          <input
+            type="text"
+            placeholder="Digite sua cidade..."
+            value={city}
+            onChange={(event) => setCity(event.target.value)}
+          />
 
-          <label>
-            Endereço
-          </label>
-          <input type="text" placeholder="Digite seu endereço..."/>
+          <label>Endereço</label>
+          <input
+            type="text"
+            placeholder="Digite seu endereço..."
+            value={address}
+            onChange={(event) => setAddress(event.target.value)}
+          />
 
-          <label>
-            Whatsapp
-          </label>
-          <input type="number" placeholder="(xx) xxxxx-xxxx"/>
+          <label>Whatsapp</label>
+          <input
+            type="number"
+            placeholder="(xx) xxxxx-xxxx"
+            value={contact}
+            onChange={(event) => setContact(event.target.value)}
+          />
 
-          <label>
-            Sobre
-          </label>
-          <textarea id="" name="sobre" placeholder="Descreva seu camping..." cols={5} rows={8} />
+          <label>Sobre</label>
+          <textarea
+            id=""
+            name="sobre"
+            placeholder="Descreva seu camping..."
+            cols={5}
+            rows={8}
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        <button type="submit">Cadastrar</button>
         </form>
 
-        <button type="submit">Cadastrar</button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Register;

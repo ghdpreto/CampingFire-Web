1bb61ead6d3a9095f4807085472239fb023152ac
ghdpreto <ghdpreto@gmail.com>
2020-10-24 22:31:51 -0300

    FInalizado listagem e retorno notfound

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -14,12 +14,40 @@ import { Camp } from '../models/CampModel';
 
 function HomePage() {
   const [camps, setCamps] = useState<Camp[]>([]);
+  const [name, setName] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  function searchCamp(value: string) {
+    if (value === '') {
+      console.log('Digite um valor');
+    } else {
+      api
+        .get(`campings/${value}`)
+        .then((response) => {
+          return setCamps(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+
+  function loadCamps() {
+    api
+      .get('campings')
+      .then((response) => {
+        // passando os valores para a variavel
+        setCamps(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   useEffect(() => {
-    api.get('camps').then(response => {
-      setCamps(response.data);
-    });
-  },[]);
+    loadCamps();
+  }, []);
 
   return (
     <div id="page-home">
@@ -27,24 +55,41 @@ function HomePage() {
         <img src={LogoImg} className="logo-camping-fire" alt="Camping Fire" />
       </Link>
 
-      <Link to="/Register">
+      <Link to="/register">
         <button className="btn-register">Cadastrar Camping</button>
       </Link>
 
       <div className="search">
-        <form>
-          <input type="search" placeholder="Pesquisar Camping" />
-          <button type="submit">Pesquisar</button>
-        </form>
+        <input
+          type="search"
+          placeholder="Pesquisar Camping"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            searchCamp(name);
+          }}
+        >
+          Pesquisar
+        </button>
       </div>
 
       <div className="container">
         <div className="cards">
-          {camps?.map((camp) => (
-            <Link to={`/camping/${camp._id}`} className="link-to">
-              <CampItem key={camp._id} camp={camp}/>
-            </Link>
-          ))}
+
+          {isLoading ? (
+            <h1>Carregando...</h1>
+          ) : !camps.length ? (
+            <span>Nenhum Camping localizado =(</span>
+          ) : (
+            camps?.map((camp) => (
+              <Link to={`/camping/${camp._id}`} className="link-to">
+                <CampItem key={camp._id} camp={camp} />
+              </Link>
+            ))
+          )}
         </div>
       </div>
     </div>

30082fd3a462543eb22f04030b7b8629b8c52233
ghdpreto <ghdpreto@gmail.com>
2020-10-24 22:30:34 -0300

    Ajuste no layout e integração da api

--- src/components/CampItem/CampItem.tsx
+++ src/components/CampItem/CampItem.tsx
@@ -1,45 +1,36 @@
 import React from 'react';
 
 // CSS
-import "./CampItem.css";
+import './CampItem.css';
 
 //ICONS
-import { BsFillTrashFill, BsPencil } from "react-icons/bs";
 
 //IMAGES
-import CampImage from "../../assets/images/imagem01.png";
-import { Link } from 'react-router-dom';
+import CampImage from '../../assets/images/imagem01.png';
 import { Camp } from '../../models/CampModel';
 
-
 interface CampItemProps {
-  camp: Camp
+  camp: Camp;
 }
 
 function CampItem(props: CampItemProps) {
-  const {camp} = props
+  const { camp } = props;
 
   return (
     <div className="card">
-      <img src={camp.image} alt="Camping"/>
+      <img src={camp.image ? camp.image : CampImage} alt="Camping" />
       <div className="content">
-  <h4>{camp.name}</h4>
-  <p>{camp.location.address}</p>
-        <p>{camp.location.city} - {camp.location.state}</p>
+        <h4>{camp.name}</h4>
+        <p>{camp.location.address}</p>
+        <p>
+          {camp.location.city} - {camp.location.state}
+        </p>
       </div>
       <div className="btn-group">
-        <button className="trash">
-          <BsFillTrashFill size="24"/>
-        </button>
-
-        <Link to="/EditCamping">
-          <button className="edit">
-            <BsPencil size="24"/>
-          </button>
-        </Link>
+        <span>Ver mais...</span>
       </div>
     </div>
-  )
+  );
 }
 
 export default CampItem;

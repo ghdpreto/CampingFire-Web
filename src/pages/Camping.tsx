f0612b4384568fbc551a501af395549f8e667bda
ghdpreto <ghdpreto@gmail.com>
2020-10-24 23:05:43 -0300

    Remoçao de imports não utilizados, add mensagem de carregamento

--- src/pages/Camping.tsx
+++ src/pages/Camping.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, Route, useHistory, useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { BsFillTrashFill, BsPencil } from 'react-icons/bs';
 
 // CSS STYLES
@@ -21,30 +21,30 @@ function Camping() {
 
   const params = useParams<CampParams>();
 
-  function deleteCamp(id: number ) {
-    console.log(id)
-    api.delete(`campings/${id}`).then(() => {
-      return (
-        alert(`Camping ${camp?.name}, foi deletado!`),
-        goBack.push('/')
-      )
-    }).catch((error) => {
-      console.log(error)
-    })
+  function deleteCamp(id: number) {
+    console.log(id);
+    api
+      .delete(`campings/${id}`)
+      .then(() => {
+        return alert(`Camping ${camp?.name}, foi deletado!`), goBack.push('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
+  function loadCamp(id: string) {
+    api
+      .get(`campings/${id}`)
+      .then((response) => {
+        setCamp(response.data);
+      })
+      .catch((error) => console.log(error));
+  }
 
   useEffect(() => {
-    api.get(`campings/${params.id}`).then((response) => {
-      setCamp(response.data);
-      console.log(response)
-    });
+    loadCamp(params.id);
   }, [params.id]);
-
-  // tela de loading (pendente)
-  if (!camp) {
-    return <h1>Carregando</h1>;
-  }
 
   return (
     <div id="page-camping">
@@ -57,30 +57,34 @@ function Camping() {
       </Link>
 
       <div className="container">
-        <div className="card">
-          <img src={camp.image} alt="Camping" />
-          <div className="content">
-            <h4>{camp.name}</h4>
-            <p>{camp.location.address}</p>
-            <p>
-              {camp.location.city} - {camp.location.state}
-            </p>
+        {!camp ? (
+          <h1>Carregando</h1>
+        ) : (
+          <div className="card">
+            <img src={camp.image} alt="Camping" />
+            <div className="content">
+              <h4>{camp.name}</h4>
+              <p>{camp.location.address}</p>
+              <p>
+                {camp.location.city} - {camp.location.state}
+              </p>
 
-            <p>{camp.description}</p>
-            <p>Contato: {camp.contact}</p>
-          </div>
-          <div className="btn-group">
-            <button className="trash" onClick={() => deleteCamp(camp._id)}>
-              <BsFillTrashFill size="24" />
-            </button>
-
-            <Link to={`/edit-camping/${camp._id}`}>
-              <button className="edit">
-                <BsPencil size="24" />
+              <p>{camp.description}</p>
+              <p>Contato: {camp.contact}</p>
+            </div>
+            <div className="btn-group">
+              <button className="trash" onClick={() => deleteCamp(camp._id)}>
+                <BsFillTrashFill size="24" />
               </button>
-            </Link>
+
+              <Link to={`/edit-camping/${camp._id}`}>
+                <button className="edit">
+                  <BsPencil size="24" />
+                </button>
+              </Link>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
